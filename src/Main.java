e2b221976cee53b0bf8c698df730948747fca8e2
Nall99 <Nallvixviii@gmail.com>
2025-08-19 23:26:07 -0300

    main

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import algoritmo1.Fatorial;
+
+public class Main {
+    public static void main(String[] args) {
+        Fatorial fac = new Fatorial();
+        System.out.println(fac.fatorial(3));
+    }
+}
